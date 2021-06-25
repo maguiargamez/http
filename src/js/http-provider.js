1c3af3610c0d13885f94f3d81a811d7b4e0ed6b8
[Manuel] <[manuelaguiargamez@gmail.com]>
2021-06-25 15:49:54 -0500

    first commit

---
+++ src/js/http-provider.js
@@ -0,0 +1,15 @@
+const jokeUrl = 'https://api.chucknorris.io/jokes/random';
+
+export const getJoke = async () => {
+
+    try{
+        const response = await fetch( jokeUrl );
+        if( !response.ok ) return alert('No se pudo realizar la peticion');  
+        const { icon_url, id, value } = await response.json();
+        return { icon_url, id, value };
+    }catch( err ){
+        throw err;
+    }  
+    
+
+}
